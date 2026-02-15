d34dfbb38689d88cc87d3b514f1dd4cb722575f6
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-15 18:41:21 +0530

    completed-custom-pipe-first-letter

--- src/app/component/one/one.ts
+++ src/app/component/one/one.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CaptializeFirstLetter } from '../../custom-pipe/captializeFirstLetter.pipe';
 
 @Component({
   selector: 'app-one',
-  imports: [],
+  imports: [FormsModule,CaptializeFirstLetter],
   templateUrl: './one.html',
   styleUrl: './one.css',
 })
-export class One {
+export class One implements OnInit{
+    myName:string = '';
+    constructor(){}
 
+    ngOnInit(): void {
+     
+    }
 }
