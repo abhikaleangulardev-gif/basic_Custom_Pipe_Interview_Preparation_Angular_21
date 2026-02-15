d0c6133f242cbdfa0eda41fcbe284f54806fb3b6
abhishek.kale <abhisahebpatil@gmail.com>
2026-02-15 19:11:15 +0530

    completed-salary-format-pipe

--- src/app/component/four/four.ts
+++ src/app/component/four/four.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SalaryFormatPipe } from '../../custom-pipe/salary-format.pipe';
 
 @Component({
   selector: 'app-four',
-  imports: [],
+  imports: [CommonModule, FormsModule, SalaryFormatPipe],
   templateUrl: './four.html',
   styleUrl: './four.css',
 })
 export class Four {
-
+  empSalary: number = 75000;
 }
